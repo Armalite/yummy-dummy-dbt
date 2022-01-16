4d68e7366c2c5c691d7e24bc91800843cc2a703d
armalite <ttc_rulz@hotmail.com>
2022-01-16 21:58:29 +1300

    Fixing sql errors

--- models/testmodel/testmodel_1.sql
+++ models/testmodel/testmodel_1.sql
@@ -1,17 +1,15 @@
 
 /*
-    This is a rather yummy dummy dbt model
+    This is a yummy dummy dbt model
 */
 
 {{ config(materialized='table') }}
 
 with source_data as (
 
-    select "This is rather" as id
-    union all
-    select "Yummy" as id
-    union all
-    select "Dummy" as id
+     select 1 as id
+     union all
+     select null as id
 
 )
 
